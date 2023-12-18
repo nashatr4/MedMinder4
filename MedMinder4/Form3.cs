1091e8fd9ed1556f1d38919404337e72089d0cf2
nashatr4 <nashatra.aqila@gmail.com>
2023-12-18 16:14:24 +0700

    connect page 3 to the database

--- MedMinder4/Form3.cs
+++ MedMinder4/Form3.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace MedMinder4
 {
@@ -15,6 +17,22 @@ namespace MedMinder4
         public Form3()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=LAPTOP-HJSTJR2S\\SQLEXPRESS;Initial Catalog=MedMinder;Integrated Security=True;Encrypt=False");
+            SqlCommand cmd = new SqlCommand(@"INSERT INTO [dbo].[Medicine1]
+           ([Medicine Name]
+           ,[Consumption Time]
+           ,[Initial Time]
+           ,[Amount of Medicine])
+     VALUES
+           ('"+textBoxMedName.Text+"', '"+numericUpDownConsumptionTime.Value+"', '"+dateTimePickerInitialTime.Value+"', '"+numericUpDownAmountofMedicine.Value+"')", conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Medicine Added");
         }
     }
 }
