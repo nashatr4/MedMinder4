da9d91e8d8ab8a5f2d0bef389939a37728895ac5
Della Febi Alfian <dellafebialfian@365.ugm.ac.id>
2023-12-20 18:50:29 +0700

    add form 1 interface

--- MedMinder4/Form1.Designer.cs
+++ MedMinder4/Form1.Designer.cs
@@ -34,13 +34,21 @@
             this.iconButton2 = new FontAwesome.Sharp.IconButton();
             this.iconButton1 = new FontAwesome.Sharp.IconButton();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.btnHome = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.panelBar = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnHome)).BeginInit();
+            this.panelBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
             this.panel1.Controls.Add(this.iconButton4);
             this.panel1.Controls.Add(this.iconButton3);
             this.panel1.Controls.Add(this.iconButton2);
@@ -54,7 +62,7 @@
             // 
             // iconButton4
             // 
-            this.iconButton4.BackColor = System.Drawing.Color.MidnightBlue;
+            this.iconButton4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
             this.iconButton4.Dock = System.Windows.Forms.DockStyle.Top;
             this.iconButton4.FlatAppearance.BorderSize = 0;
             this.iconButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
@@ -76,7 +84,7 @@
             // 
             // iconButton3
             // 
-            this.iconButton3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.iconButton3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
             this.iconButton3.Dock = System.Windows.Forms.DockStyle.Top;
             this.iconButton3.FlatAppearance.BorderSize = 0;
             this.iconButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
@@ -98,7 +106,7 @@
             // 
             // iconButton2
             // 
-            this.iconButton2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.iconButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
             this.iconButton2.Dock = System.Windows.Forms.DockStyle.Top;
             this.iconButton2.FlatAppearance.BorderSize = 0;
             this.iconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
@@ -120,7 +128,7 @@
             // 
             // iconButton1
             // 
-            this.iconButton1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.iconButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
             this.iconButton1.Dock = System.Windows.Forms.DockStyle.Top;
             this.iconButton1.FlatAppearance.BorderSize = 0;
             this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
@@ -142,13 +150,25 @@
             // 
             // panel2
             // 
-            this.panel2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.panel2.Controls.Add(this.btnHome);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(220, 140);
             this.panel2.TabIndex = 0;
+            // 
+            // btnHome
+            // 
+            this.btnHome.Image = global::MedMinder4.Properties.Resources.LOGO_MEDMINDER;
+            this.btnHome.Location = new System.Drawing.Point(0, 43);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(220, 79);
+            this.btnHome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnHome.TabIndex = 0;
+            this.btnHome.TabStop = false;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
             // 
             // button1
             // 
@@ -160,17 +180,58 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // panelBar
+            // 
+            this.panelBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            this.panelBar.Controls.Add(this.label1);
+            this.panelBar.Controls.Add(this.iconPictureBox1);
+            this.panelBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelBar.Location = new System.Drawing.Point(220, 0);
+            this.panelBar.Name = "panelBar";
+            this.panelBar.Size = new System.Drawing.Size(580, 80);
+            this.panelBar.TabIndex = 2;
+            this.panelBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelBar_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.PeachPuff;
+            this.label1.Location = new System.Drawing.Point(60, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Home";
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            this.iconPictureBox1.ForeColor = System.Drawing.Color.PeachPuff;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.HouseChimneyMedical;
+            this.iconPictureBox1.IconColor = System.Drawing.Color.PeachPuff;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.Location = new System.Drawing.Point(22, 27);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.iconPictureBox1.TabIndex = 0;
+            this.iconPictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelBar);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnHome)).EndInit();
+            this.panelBar.ResumeLayout(false);
+            this.panelBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -184,6 +245,10 @@
         private FontAwesome.Sharp.IconButton iconButton1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox btnHome;
+        private System.Windows.Forms.Panel panelBar;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- MedMinder4/Form1.cs
+++ MedMinder4/Form1.cs
@@ -4,11 +4,13 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Media;
 using FontAwesome.Sharp;
+
 
 namespace MedMinder4
 {
@@ -24,39 +26,43 @@ namespace MedMinder4
             InitializeComponent();
             leftBorderBtn = new Panel();
             leftBorderBtn.Size = new Size(10, 60);
-            panel2.Controls.Add(leftBorderBtn);
+            panel1.Controls.Add(leftBorderBtn);
+            //Form
+            this.Text = string.Empty;
+            this.ControlBox = false;
+            this.DoubleBuffered = true;
+            this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
         }
-        
+
         private struct RGBColors
         {
-            public static Color color1 = color.fromArgb(172, 126, 241);
-            public static Color color2 = color.fromArgb(249, 118, 176);
-            public static Color color3 = color.fromArgb(253, 138, 114);
-            public static Color color4 = color.fromArgb(95, 77, 221);
-            public static Color color5 = color.fromArgb(249, 88, 155);
-            public static Color color6 = color.fromArgb(24, 262, 251);
+            public static System.Drawing.Color color1 = System.Drawing.Color.FromArgb(172, 126, 241);
+            public static System.Drawing.Color color2 = System.Drawing.Color.FromArgb(248, 118, 176);
+            public static System.Drawing.Color color3 = System.Drawing.Color.FromArgb(253, 138, 114);
+            public static System.Drawing.Color color4 = System.Drawing.Color.FromArgb(24, 242, 251);
         }
-        
-        private void ActivateButton(object senderBtn, Color color)
-        {
-            if (senderBtn != null) 
-            {
-                DisableButton()
 
+        private void ActivateButton(object senderBtn, System.Drawing.Color color)
+        {
+            if (senderBtn != null)
+            {
+                DisableButton();
                 //Button
-                currentBtn = (IconButton)senderBtn; 
-                currentBtn.BackColor = Color.AliceBlue;
+                currentBtn = (IconButton)senderBtn;
+                currentBtn.BackColor = System.Drawing.Color.FromArgb(37, 36, 81);
                 currentBtn.ForeColor = color;
-                currentBtn.TextAlign = ContentAlignment.MiddleCenter;
+                currentBtn.TextAlign = ContentAlignment.MiddleRight;
                 currentBtn.IconColor = color;
                 currentBtn.TextImageRelation = TextImageRelation.TextBeforeImage;
                 currentBtn.ImageAlign = ContentAlignment.MiddleRight;
-
                 //Left border button
                 leftBorderBtn.BackColor = color;
                 leftBorderBtn.Location = new Point(0, currentBtn.Location.Y);
                 leftBorderBtn.Visible = true;
                 leftBorderBtn.BringToFront();
+                //Icont Current Form
+                iconPictureBox1.IconChar = currentBtn.IconChar;
+                iconPictureBox1.IconColor = color;
             }
         }
 
@@ -64,10 +70,10 @@ namespace MedMinder4
         {
             if (currentBtn != null)
             {
-                currentBtn.BackColor = Color.LightSteelBlue;
-                currentBtn.ForeColor = Color.MidnightBlue;
+                currentBtn.BackColor = System.Drawing.Color.FromArgb(31, 30, 68);
+                currentBtn.ForeColor = System.Drawing.Color.AliceBlue;
                 currentBtn.TextAlign = ContentAlignment.MiddleLeft;
-                currentBtn.IconColor = Color.MidnightBlue;
+                currentBtn.IconColor = System.Drawing.Color.AliceBlue;
                 currentBtn.TextImageRelation = TextImageRelation.ImageBeforeText;
                 currentBtn.ImageAlign = ContentAlignment.MiddleLeft;
             }
@@ -82,7 +88,7 @@ namespace MedMinder4
 
         private void iconButton2_Click(object sender, EventArgs e)
         {
-            AcceptButton(sender, RGBColors.color2)
+            ActivateButton(sender, RGBColors.color2);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -92,17 +98,44 @@ namespace MedMinder4
 
         private void iconButton1_Click(object sender, EventArgs e)
         {
-            AcceptButton(sender, RGBColors.color1)
+            ActivateButton(sender, RGBColors.color1);
         }
 
         private void iconButton3_Click(object sender, EventArgs e)
         {
-            AcceptButton(sender, RGBColors.color3)
+            ActivateButton(sender, RGBColors.color3);
         }
 
         private void iconButton4_Click(object sender, EventArgs e)
         {
-            AcceptButton(sender, RGBColors.color4)
+            ActivateButton(sender, RGBColors.color4);
         }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            Reset();
+
+        }
+        private void Reset()
+        {
+            DisableButton();
+            leftBorderBtn.Visible = false;
+            iconPictureBox1.IconChar = IconChar.HouseChimneyMedical;
+            iconPictureBox1.IconColor = System.Drawing.Color.PeachPuff;
+        }
+        
+        //Drag From
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void panelBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+       
     }
 }
