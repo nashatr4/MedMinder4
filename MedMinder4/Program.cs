5757053445e1e1d8a31e95452fec96d6621e320f
Della Febi Alfian <dellafebialfian@365.ugm.ac.id>
2023-12-22 16:02:05 +0700

    changing the main page

--- MedMinder4/Program.cs
+++ MedMinder4/Program.cs
@@ -17,7 +17,7 @@ namespace MedMinder4
         {
             System.Windows.Forms.Application.EnableVisualStyles();
             System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
-            System.Windows.Forms.Application.Run(new Form5());
+            System.Windows.Forms.Application.Run(new Form1());
 
         }
     }
