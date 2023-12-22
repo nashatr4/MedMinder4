33e5f822938dbe427abe6157117e943e3441a037
nashatra4 <nashatra.aqila@gmail.com>
2023-12-22 22:58:05 +0700

    revise button

--- MedMinder4/Form3.Designer.cs
+++ MedMinder4/Form3.Designer.cs
@@ -44,9 +44,9 @@ namespace MedMinder4
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(572, 292);
+            this.button1.Location = new System.Drawing.Point(315, 315);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(78, 40);
+            this.button1.Size = new System.Drawing.Size(79, 37);
             this.button1.TabIndex = 0;
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = true;
